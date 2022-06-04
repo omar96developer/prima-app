852e673c06487f3a93f8c9fe693333cb65dafd58
omar96developer <omakino@hotmail.it>
2022-06-04 20:31:12 +0200

    populate dinamicly the cards

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React from "react";
-/* import Componente1 from "./Componente1"; */
 import Product from "./Product";
+
+const primaCard = {
+  nome: '5€ Amazon',
+  img: 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f11894.jpg',
+  prezzo: 2.00,
+  costofinale: 8
+}
+
+const secondaCard = {
+  nome: '50€ Amazon',
+  img: 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f11902.jpg',
+  prezzo: 0.10,
+  costofinale: 20
+}
 
 function App() {
   return (
     <div className="App">
       <h1>La nostra prima card</h1>
       <div className="container">
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-
+      <Product 
+      nome={primaCard.nome}
+      img={primaCard.img}
+      prezzo={primaCard.prezzo}
+      costofinale={primaCard.costofinale}
+      />
+      <Product 
+      nome={secondaCard.nome}
+      img={secondaCard.img}
+      prezzo={secondaCard.prezzo}
+      costofinale={secondaCard.costofinale}
+      />
       </div>
-    {/*   <Componente1 /> */}
+    
     </div>
     
   );
--- src/Product.js
+++ src/Product.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
-const img = 'https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f11894.jpg';
-const button = '8,00€'
-const Product = () => {
+const Product = (props) => {
+  console.log(props);
+
+  
+
   return (
     <article>
         
         <div className='card'>
-            <img src={img} alt='amz'/>
-            <h4>5€ Amazon</h4>
-            <p className='prezzo'>2,00€</p>
+            <img src={props.img} alt='amz'/>
+            <h4>{props.nome}</h4>
+            <p className='prezzo'>{props.prezzo}€</p>
             <p className='chi'>bamby000</p>           
             <p className='status'>RIAPRE PRESTO</p>
-            <button className='compra'>{button}</button>
+            <button className='compra'>{props.costofinale}€</button>
         </div>
         
     </article>
